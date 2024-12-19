60e4a3ec28444e843b49cc38b5c459d49a43807f
vickyaltitude <vignvick3005>
2024-12-19 16:16:58 +0530

    successfully implemented increment by 5 and decrement by 5 with redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,29 @@
 import classes from './Counter.module.css';
+import { useSelector,useDispatch } from 'react-redux';
 
 const Counter = () => {
+
+  const counter = useSelector(state => state.count)
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
 
+  function useIncrement(){
+     dispatch({type: 'increment'})
+  }
+
+  function useDecrement(){
+    dispatch({type: 'decrement'})
+  }
+  
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>-- {counter} --</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div>
+        <button onClick={useIncrement}>Increment by 5</button>
+        <button onClick={useDecrement}>Decrement by 5</button>
+      </div>
     </main>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,13 @@ const counterReducer = (state={count:0},action) =>{
 
     if(action.type === 'increment'){
         return {
-            count : state.count + 1
+            count : state.count + 5
         }
     }
 
     if(action.type === 'decrement'){
         return {
-            count : state.count - 1
+            count : state.count - 5
         }
     }
 
